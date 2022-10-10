ff96af5534b49ad19d1b6b2e723c8be727ee4690
Mr-Keks <levelel744@gmail.com>
2022-10-11 01:28:27 +0200

    add webhook

--- loader.py
+++ loader.py
@@ -1,3 +1,5 @@
+import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from environs import Env
@@ -8,3 +10,12 @@ env.read_env()
 bot = Bot(token=env('TOKEN'))
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+HEROKU_APP_NAME = env('APP_NAME')
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f"/webhook/{env('TOKEN')}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
--- run.py
+++ run.py
@@ -1,18 +1,35 @@
 import logging
 
 from aiogram import executor
-from loader import dp
+from aiogram.utils.executor import start_webhook
 
+from loader import dp, WEBHOOK_URL, WEBAPP_HOST, WEBHOOK_PATH, WEBAPP_PORT, bot
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def on_sturtup(dp):
+async def on_startup(dp):
     import hendlers
     import middlewares
 
     hendlers.setup(dp)
     middlewares.setup(dp)
 
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_sturtup)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
